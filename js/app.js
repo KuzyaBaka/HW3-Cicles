0ba90b833146fa11a8105a70b1455fb72bb10ac3
KuzyaBaka <youryz58@gmail.com>
2022-12-04 20:53:50 +0300

    Edit ex 3

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Задание 1
 
-const num1 = prompt(`Введите первое число диапазона`);
+/* const num1 = prompt(`Введите первое число диапазона`);
 const num2 = prompt(`Введите второе число диапазона`);
 
 if (+num2 - +num1 >= 5 || +num1 - +num2 >= 5) {
@@ -27,6 +27,36 @@ for (; userPassword !== entPassword; ) {
   alert(`У вас осталась 2 попытки`);
   for (; userPassword !== entPassword; ) {}
 }
-alert(`Привет`);
+alert(`Привет`); */
 
 // Задание 3
+
+const numberD1 = +prompt(`Введите первое число диапазона`);
+const numberD2 = +prompt(`Введите второе число диапазона`);
+
+for (let i = numberD1; i <= numberD2; i++) {
+  function ex3() {
+    let res1 = i > 100 ? `больше ста ` : `меньше ста `;
+    let res2 = i % 2 === 0 ? `чётное ` : `нечётное `;
+    let res3 = i % 1 === 0 ? `недробное ` : `дробное `;
+    let res4 = i > 0 ? `больше нуля ` : `меньше нуля `;
+    return res1 + res2 + res3 + res4;
+  }
+  console.log(`Ваше число ${i} ` + ex3());
+}
+
+// Задание 4
+
+const numEx4 = +prompt(`Введите первое число диапазона`);
+const numEx5 = +prompt(`Введите второе число диапазона`);
+if (+numEx4 < +numEx5) {
+  let count = 0;
+
+  for (let i = +numEx4; i <= ( + 7) ; i++) {
+    console.log(i);
+  }
+} else if (+numEx4 > +numEx5) {
+  for (let i = +numEx4; i >= ( - 7); i--) {
+    console.log(i);
+  }
+}
